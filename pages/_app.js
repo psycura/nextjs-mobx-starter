48825a64a196a96edfc1be8245310790ddc4b4b9
eugenealitz <psycura@gmail.com>
2018-09-02 16:13:19 +0300

    init

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,16 @@ import App, { Container } from 'next/app'
 import React from 'react'
 import { initStore } from '../store/rootStore';
 import { Provider } from 'mobx-react'
+
 import { ThemeProvider } from 'styled-components';
+import withApolloClient from '../lib/with-apollo-client'
+import { ApolloProvider } from 'react-apollo'
 
 const theme = {
     secondaryColor: 'red',
     primaryColor:   'green',
     borderColor:    '#ccc',
 };
-
-import './Main.scss'
 
 class MyApp extends App {
     constructor ( props ) {
@@ -21,17 +22,20 @@ class MyApp extends App {
     }
     
     render () {
-        const { Component, pageProps } = this.props;
+        const { Component, pageProps, apolloClient } = this.props;
+        
         return (
             <Container>
-                <Provider {...this.stores}>
-                    <ThemeProvider theme={theme}>
-                        <Component test='true' {...pageProps} />
-                    </ThemeProvider>
-                </Provider>
+                <ApolloProvider client={apolloClient}>
+                    <Provider {...this.stores}>
+                        <ThemeProvider theme={theme}>
+                            <Component test='true' {...pageProps} />
+                        </ThemeProvider>
+                    </Provider>
+                </ApolloProvider>
             </Container>
         )
     }
 }
 
-export default MyApp
+export default withApolloClient ( MyApp )
